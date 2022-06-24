cb09d87588243c698b1f5b9df6ef0e86919f59c3
gmhi-webtool <108020032+gmhi-webtool@users.noreply.github.com>
2022-06-24 13:06:04 -0500

    Add files via upload

---
+++ bar.js
@@ -0,0 +1,438 @@
+import { get_export_svg_link, get_export_png_link } from "./utils.js";
+
+export function plot_bar(ele, data, sample, rank) {
+  ele.innerHTML = "";
+
+  let xDomain = [data[0].pop];
+  if (sample.length != 0) {
+    xDomain = ["Sample"].concat(xDomain);
+  }
+
+  const [final_data, taxons] = preprocess(data, sample);
+  // console.log(final_data, taxons);
+
+  const bar = StackedBarChart(final_data, rank, {
+    x: (d) => d.pop,
+    y: (d) => d.abundance,
+    z: (d) => d.taxon,
+    xDomain,
+    zDomain: taxons,
+    colors: d3.schemeSpectral[taxons.length],
+    width: 740,
+    height: 500,
+  });
+
+  ele.appendChild(bar);
+  const caption = get_caption(data[0].pop, rank, sample);
+  ele.innerHTML += caption;
+  let a = get_export_svg_link(`${rank}-Stacked-bar`, bar);
+  ele.appendChild(a);
+  ele.insertAdjacentHTML('beforeend', " ");
+  a = get_export_png_link(`${rank}-Stacked-bar`, ele);
+  ele.appendChild(a);
+
+  // hover events
+  for (const tax of final_data.map((ele) => ele["taxon"])) {
+    d3.selectAll(`#${rank}_${tax}_bar`)
+      .on("mouseover", function () {
+        handle_mouseover(tax, taxons, rank);
+      })
+      .on("mouseout", function () {
+        handle_mouseout(tax, taxons, rank);
+      });
+
+    // d3.selectAll(`#${tax}_square`)
+    // .on("mouseover", function(){handle_mouseover(tax, taxons)})
+    // .on("mouseout", function(){handle_mouseout(tax, taxons)});
+
+    d3.selectAll(`#${rank}_${tax}_square-text`)
+      .on("mouseover", function () {
+        handle_mouseover(tax, taxons, rank);
+      })
+      .on("mouseout", function () {
+        handle_mouseout(tax, taxons, rank);
+      });
+  }
+}
+
+const preprocess = (data, sample) => {
+  let final_data;
+  let taxons;
+  if (sample.length == 0) {
+    const data_copy = [...data];
+    data_copy.sort(sort_func);
+
+    const num_taxons = Math.min(10, data_copy.length);
+    const reduced_data = data_copy.slice(0, num_taxons);
+
+    taxons = reduced_data.map((ele) => ele.taxon);
+
+    const data_sum = reduced_data.reduce(
+      (prev, curr) => prev + curr.abundance,
+      0
+    );
+
+    const reduced_data_filled = [
+      ...reduced_data,
+      { pop: reduced_data[0].pop, taxon: `Others`, abundance: 1 - data_sum },
+    ];
+
+    final_data = reduced_data_filled;
+    taxons = taxons.concat(["Others"]);
+  } else {
+    const data_copy = [...data];
+    const sample_copy = [...sample];
+    data_copy.sort(sort_func);
+    sample_copy.sort(sort_func);
+
+    // get the 10 most abundant taxons from the sample
+    const num_taxons = Math.min(10, sample_copy.length);
+    const reduced_sample = sample_copy.slice(0, num_taxons);
+
+    // retrieve those same taxons from the dataset (average)
+    taxons = reduced_sample.map((ele) => ele.taxon);
+    let reduced_data = data_copy.filter((ele) => taxons.includes(ele.taxon));
+    const reduced_data_taxons = reduced_data.map((ele) => ele.taxon);
+
+    // If dataset does not contain one of the taxons that the sample has
+    // Add a dummy sample to dataset
+    reduced_data = reduced_data.concat(
+      taxons.filter((ele) => !reduced_data_taxons.includes(ele))
+        .map((taxon) => ({pop : data_copy[0]["pop"], taxon, abundance : 0}))
+    );
+
+    // Add a dummy taxon, others, such that the sum of each column is 1
+    const sample_sum = reduced_sample.reduce(
+      (prev, curr) => prev + curr.abundance,
+      0
+    );
+    const data_sum = reduced_data.reduce(
+      (prev, curr) => prev + curr.abundance,
+      0
+    );
+
+    const reduced_sample_filled = [
+      ...reduced_sample,
+      { pop: "Sample", taxon: "Others", abundance: 1 - sample_sum },
+    ];
+    const reduced_data_filled = [
+      ...reduced_data,
+      { pop: reduced_data[0].pop, taxon: "Others", abundance: 1 - data_sum },
+    ];
+
+    // Get the final dataset
+    final_data = reduced_data_filled.concat(reduced_sample_filled);
+    taxons = taxons.concat(["Others"]);
+  }
+
+  // Sort the taxons in alphabetical order (but "Others" is first)
+  const alpha = (a, b) => {
+    if (a === "Others") return 1;
+    if (a < b) return 1;
+    if (a > b) return -1;
+    return 0;
+  }
+  taxons.sort(alpha);
+
+  return [final_data, taxons];
+};
+
+const handle_mouseover = (taxon, taxons, rank) => {
+  // highlight taxon
+  for (const tax of taxons) {
+    if (tax !== taxon) {
+      // d3.selectAll(`#${tax}_bar`).attr("opacity", "0.5")
+      // d3.selectAll(`#${tax}_square`).attr("opacity", "0.5")
+      d3.selectAll(`#${rank}_${tax}_bar`).attr("filter", "brightness(50%)");
+      d3.selectAll(`#${rank}_${tax}_square`).attr("filter", "brightness(50%)");
+      d3.selectAll(`#${rank}_${tax}_square-text`).attr("opacity", "0.5");
+    }
+  }
+  d3.selectAll(`#${rank}_${taxon}_text`).attr("opacity", "1");
+};
+
+const handle_mouseout = (taxon, taxons, rank) => {
+  // unhighlight taxon
+  for (const tax of taxons) {
+    // d3.selectAll(`#${tax}_bar`).attr("opacity", "1")
+    // d3.selectAll(`#${tax}_square`).attr("opacity", "1")
+    d3.selectAll(`#${rank}_${tax}_bar`).attr("filter", "brightness(100%)");
+    d3.selectAll(`#${rank}_${tax}_square`).attr("filter", "brightness(100%)");
+    d3.selectAll(`#${rank}_${tax}_square-text`).attr("opacity", "1");
+  }
+  d3.selectAll(`#${rank}_${taxon}_text`).attr("opacity", "0");
+};
+
+const get_caption = (pop, rank, sample) => {
+  const desc = {
+    "Average Healthy": "2754 healthy",
+    "Average Nonhealthy": "2272 nonhealthy",
+    Average: "5026 healthy and nonhealthy",
+  };
+  let string;
+  const figname = "Taxonomic Distribution";
+  if (sample.length == 0) {
+    string = `<br/><br/><b>${figname}. </b> Distribution of the average relative abundances of the gut microbes of ${desc[pop]} persons at the ${rank} level. `;
+  } else {
+    string = `<br/><br/><b>${figname}. </b> Distribution of the relative abundances of the input sample microbes (left) and average relative abundances of the gut microbes of ${desc[pop]} persons (right) at the ${rank} level. `;
+  }
+  return string;
+};
+
+const sort_func = function (a, b) {
+  const keyA = a.abundance;
+  const keyB = b.abundance;
+  if (keyA < keyB) return 1;
+  if (keyA > keyB) return -1;
+  return 0;
+};
+
+function StackedBarChart(
+  data,
+  rank,
+  {
+    x = (d, i) => i, // given d in data, returns the (ordinal) x-value
+    y = (d) => d, // given d in data, returns the (quantitative) y-value
+    z = () => 1, // given d in data, returns the (categorical) z-value
+    title, // given d in data, returns the title text
+    marginTop = 30, // top margin, in pixels
+    marginRight = 30, // right margin, in pixels
+    marginBottom = 30, // bottom margin, in pixels
+    marginLeft = 70, // left margin, in pixels
+    width = 640, // outer width, in pixels
+    height = 500, // outer height, in pixels
+    xDomain, // array of x-values
+    // xRange = [marginLeft, width - marginRight], // [left, right]
+    xPadding = 0.05, // amount of x-range to reserve to separate bars
+    yType = d3.scaleLinear, // type of y-scale
+    yDomain, // [ymin, ymax]
+    yRange = [height - marginBottom, marginTop], // [bottom, top]
+    zDomain, // array of z-values
+    offset = d3.stackOffsetDiverging, // stack offset method
+    order = d3.stackOrderNone, // stack order method
+    yFormat, // a format specifier string for the y-axis
+    yLabel, // a label for the y-axis
+    colors = d3.schemeTableau10, // array of colors
+  } = {}
+) {
+  const graph_width = 0.5 * width;
+  const xRange = [marginLeft, graph_width - marginRight]; // [left, right]
+
+  // Compute values.
+  const X = d3.map(data, x);
+  const Y = d3.map(data, y);
+  const Z = d3.map(data, z);
+
+  // Compute default x- and z-domains, and unique them.
+  if (xDomain === undefined) xDomain = X;
+  if (zDomain === undefined) zDomain = Z;
+  xDomain = new d3.InternSet(xDomain);
+  const taxons = zDomain;
+  zDomain = new d3.InternSet(zDomain);
+
+  // Omit any data not present in the x- and z-domains.
+  const I = d3
+    .range(X.length)
+    .filter((i) => xDomain.has(X[i]) && zDomain.has(Z[i]));
+
+  // Compute a nested array of series where each series is [[y1, y2], [y1, y2],
+  // [y1, y2], …] representing the y-extent of each stacked rect. In addition,
+  // each tuple has an i (index) property so that we can refer back to the
+  // original data point (data[i]). This code assumes that there is only one
+  // data point for a given unique x- and z-value.
+  const series = d3
+    .stack()
+    .keys(zDomain)
+    .value(([x, I], z) => Y[I.get(z)])
+    .order(order)
+    .offset(offset)(
+      d3.rollup(
+        I,
+        ([i]) => i,
+        (i) => X[i],
+        (i) => Z[i]
+      )
+    )
+    .map((s) => s.map((d) => Object.assign(d, { i: d.data[1].get(s.key) })));
+
+  // Compute the default y-domain. Note: diverging stacks can be negative.
+  // if (yDomain === undefined) yDomain = d3.extent(series.flat(2));
+  yDomain = [0, 1];
+
+  // Construct scales, axes, and formats.
+  const xScale = d3
+    .scaleBand(xDomain, xRange)
+    .paddingInner(xPadding)
+    .paddingOuter(xPadding);
+  const yScale = yType(yDomain, yRange);
+  const color = d3.scaleOrdinal(zDomain, colors);
+  const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
+  const yAxis = d3.axisLeft(yScale).ticks(height / 60, d3.format(",%"));
+
+  // Compute titles.
+  if (title === undefined) {
+    const formatValue = yScale.tickFormat(100, yFormat);
+    title = (i) => `${X[i]}\n${Z[i]}\n${formatValue(Y[i])}`;
+  } else {
+    const O = d3.map(data, (d) => d);
+    const T = title;
+    title = (i) => T(O[i], i, data);
+  }
+
+  const svg = d3
+    .create("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height])
+    .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
+    .attr("id", `bar-${rank}`);
+
+  svg
+    .append("g")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(yAxis)
+    .style("font-family", "latin-modern")
+    .call((g) => g.select(".domain").remove())
+    .call((g) =>
+      g
+        .selectAll(".tick line")
+        .clone()
+        .attr("x2", graph_width - marginLeft - marginRight)
+        .attr("stroke-opacity", 0.1)
+    )
+    .style("stroke-dasharray", "3, 3");
+
+  const bar = svg
+    .append("g")
+    .selectAll("g")
+    .data(series)
+    .join("g")
+    .attr("fill", ([{ i }]) => color(Z[i]))
+    .selectAll("rect")
+    .data((d) => d)
+    .join("rect")
+    .attr("x", ({ i }) => xScale(X[i]))
+    .attr("y", ([y1, y2]) => Math.min(yScale(y1), yScale(y2)))
+    .attr("height", ([y1, y2]) => Math.abs(yScale(y1) - yScale(y2)))
+    .attr("width", xScale.bandwidth())
+    .attr("id", ({ i }) => `${rank}_${data[i]["taxon"]}_bar`);
+
+  if (title) bar.append("title").text(({ i }) => title(i));
+
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${yScale(0)})`)
+    .call(xAxis)
+    .style("font-family", "latin-modern")
+    .style("font-size", "1em");
+
+  // border box
+  svg
+    .append("rect")
+    .attr("x", marginLeft)
+    .attr("y", marginTop)
+    .attr("height", height - marginTop - marginBottom)
+    .attr("width", graph_width - marginLeft - marginRight)
+    .style("stroke", "black")
+    .style("fill", "none")
+    .style("stroke-width", 1);
+
+  // y label
+  svg
+    .append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "middle")
+    .attr("x", -height / 2)
+    .attr("y", 20)
+    .text("Relative Abundance")
+    .attr("transform", function (d) {
+      return "rotate(-90)";
+    });
+
+  const x_start = graph_width + 50;
+  const y_start = 100;
+  const space = 20;
+  const size = 10;
+
+  svg
+    .append("text")
+    .attr("x", x_start - 10)
+    .attr("y", y_start - 30)
+    .text(rank[0].toUpperCase() + rank.slice(1))
+    .style("font-size", "20px")
+    .attr("font-weight", "900")
+    .attr("alignment-baseline", "middle")
+
+  for (let i = taxons.length - 1; i >= 0; i--) {
+    const x = x_start;
+    const y = y_start + (taxons.length - 1 - i) * space;
+
+    // color square
+    svg
+      .append("rect")
+      .attr("x", x - size)
+      .attr("y", y - 0.5 * size)
+      .attr("width", size)
+      .attr("height", size)
+      .style("fill", colors[i])
+      .style("stroke", "black")
+      .style("stroke-width", 1)
+      .attr("id", `${rank}_${taxons[i]}_square`);
+
+    const text =
+      taxons[i] != "Others" ? taxons[i].slice(3, taxons[i].length) : "Others";
+    svg
+      .append("text")
+      .attr("x", x + 10)
+      .attr("y", y + 2)
+      .text(text)
+      .style("font-size", "15px")
+      .attr("alignment-baseline", "middle")
+      .attr("id", `${rank}_${taxons[i]}_square-text`);
+
+    const x_tool = x;
+    const y_tool = y_start + (1.2 * taxons.length - 1) * space + 20;
+
+    // tooltip
+
+    const average = data.filter(
+      (ele) => ele["pop"] !== "Sample" && ele["taxon"] == taxons[i]
+    );
+
+    const average_text = `${average[0]["pop"]}: ${(
+      average[0]["abundance"] * 100
+    ).toFixed(2)}%`;
+
+    svg
+      .append("text")
+      .attr("x", x_tool)
+      .attr("y", y_tool)
+      .text(average_text)
+      .style("font-size", "15px")
+      .attr("alignment-baseline", "middle")
+      .attr("opacity", "0")
+      .attr("id", `${rank}_${taxons[i]}_text`);
+
+    const sample = data.filter(
+      (ele) => ele["pop"] === "Sample" && ele["taxon"] == taxons[i]
+    );
+
+    if (sample.length === 0) {
+      continue;
+    }
+
+    const sample_text = `Sample: ${(sample[0]["abundance"] * 100).toFixed(2)}%`;
+
+    svg
+      .append("text")
+      .attr("x", x_tool)
+      .attr("y", y_tool + 20)
+      .text(sample_text)
+      .style("font-size", "15px")
+      .attr("alignment-baseline", "middle")
+      .attr("opacity", "0")
+      .attr("id", `${rank}_${taxons[i]}_text`);
+  }
+
+  return Object.assign(svg.node(), { scales: { color } });
+}
---
+++ utils.js
@@ -0,0 +1,349 @@
+import { indicies } from "./indicies.js";
+import { index_data, gmhi_model } from "./data.js";
+
+export function get_export_svg_link(name, svg) {
+  const a = document.createElement('a');
+  a.setAttribute("href", "#!");
+  a.innerText = "Export as .svg";
+
+  a.onclick = () => {
+    console.log("hi");
+    const b = get_svg_blob(svg);
+    saveAs(b, name + ".svg");
+  }
+  return a;
+}
+
+export function get_export_png_link(name, ele) {
+  const a = document.createElement('a');
+  a.setAttribute("href", "#!");
+  a.innerText = "Export as .png";
+
+  a.onclick = () => {
+    export_plot(a, ele, name)
+  }
+  return a;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function export_plot(self, ele, name) {
+  for (const ele of document.querySelectorAll("*")) {
+    ele.classList.add("wait");
+  }
+  // self.innerText = "Exporting..."
+  console.log("done setting css");
+  await sleep(100);
+  console.log("done sleeping");
+  console.log("Start export");
+  const start = performance.now();
+  const pca = document.getElementById("pca");
+  const isPCA = pca.contains(ele);
+  html2canvas(ele, {scale : 8,
+    ignoreElements : function(element) {
+      const ignore = !isPCA && (element.id == pca || pca.contains(element));
+      return ignore;
+      // const ignore = element.id !== ele.id && !ele.contains(element) && !element.contains(ele);
+      // return ignore;
+    },
+    onclone : function(doc) {
+      const eles = doc.getElementsByTagName("a");
+      for (const e of eles) {
+        e.innerHTML = "";
+      }
+    }  
+  }).then(
+    function(canvas) {
+      console.log("Finished canvas conversion");
+      const w = canvas.width;
+      const h = canvas.height;
+      const img = canvas.toBlob(function(blob){
+        saveAs(blob, name + ".png");
+        for (const ele of document.querySelectorAll("*")) {
+          ele.classList.remove("wait");
+        }
+        // self.innerText = "Export me!"
+      });
+  })
+}
+
+export const get_svg_blob = (svgEl) => {
+  // svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  var svgData = svgEl.innerHTML;
+  var head = '<svg title="graph" version="1.1" xmlns="http://www.w3.org/2000/svg">';
+
+  const style = `<style> .footer {
+    color: #ccc;
+    /* position: fixed; */
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+  }
+  
+  #sideButton {
+    position: sticky !important;
+    top: 0 !important;
+  }
+  
+  .tick {
+    font-family : "Latin Modern" !important;
+  }
+  
+  .bottom-link {
+    color: inherit;
+  }
+  
+  .hide {
+    z-index: -5;
+  }
+  
+  .btn-close {
+    margin-right: 10px;
+    margin-top: 10px;
+  }
+  
+  red {
+    color: #a00 !important;
+  }
+  
+  #example {
+    color: #a00 !important;
+    text-decoration: underline;
+  }
+  
+  * {
+    box-shadow: none !important;
+  }
+  
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  *::-webkit-scrollbar {
+    display: none;
+  }
+  
+  /* Hide scrollbar for IE, Edge and Firefox */
+  * {
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+  }
+  
+  header {
+    margin-bottom: 100px;
+  }
+  
+  .setting {
+    margin-bottom: 80px;
+  }
+  
+  .fig {
+    margin-bottom: 150px;
+  }
+  
+  /* latex font */
+  
+  @font-face {
+    font-family: "Latin Modern";
+    font-style: normal;
+    font-weight: normal;
+    font-display: swap;
+    src: url("https://raw.githubusercontent.com/vincentdoerig/latex-css/master/fonts/LM-regular.woff2")
+        format("woff2"),
+      url("https://raw.githubusercontent.com/vincentdoerig/latex-css/master/fonts/LM-regular.woff")
+        format("woff"),
+      url("https://raw.githubusercontent.com/vincentdoerig/latex-css/master/fonts/LM-regular.ttf")
+        format("truetype");
+  }
+  
+  @font-face {
+    font-family: "Latin Modern";
+    font-style: normal;
+    font-weight: bold;
+    font-display: swap;
+    src: url("https://raw.githubusercontent.com/vincentdoerig/latex-css/master/fonts/LM-bold.woff2")
+        format("woff2"),
+      url("https://raw.githubusercontent.com/vincentdoerig/latex-css/master/fonts/LM-bold.woff")
+        format("woff"),
+      url("https://raw.githubusercontent.com/vincentdoerig/latex-css/master/fonts/LM-bold.ttf")
+        format("truetype");
+  }
+  
+  body {
+    font-family: "Latin Modern", Georgia, Cambria, "Times New Roman", Times, serif;
+    overscroll-behavior-y: none;
+  }
+  
+  
+  /* table stuff */
+  
+  /* Better tables */
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    width: auto;
+    max-width: 100%;
+    border-top: 2.27px solid black;
+    border-bottom: 2.27px solid black;
+    /* display: block; */
+    overflow-x: auto; /* does not work because element is not block */
+    /* white-space: nowrap; */
+    counter-increment: caption;
+  }
+  /* add bottom border on column table headings  */
+  table tr > th[scope='col'] {
+    border-bottom: 1.36px solid black;
+  }
+  /* add right border on row table headings  */
+  table tr > th[scope='row'] {
+    border-right: 1.36px solid black;
+  }
+  table > tbody > tr:first-child > td,
+  table > tbody > tr:first-child > th {
+    border-top: 1.36px solid black;
+  }
+  table > tbody > tr:last-child > td,
+  table > tbody > tr:last-child > th {
+    border-bottom: 1.36px solid black;
+  }
+  
+  th,
+  td {
+    text-align: left;
+    padding: 0.5rem;
+    line-height: 1.1;
+  }
+  /* Table caption */
+  caption {
+    text-align: left;
+    font-size: 0.923em;
+    /* border-bottom: 2pt solid #000; */
+    padding: 0 0.25em 0.25em;
+    width: 100%;
+    margin-left: 0;
+  }
+  
+  caption::before {
+    content: 'Table ' counter(caption) '. ';
+    font-weight: bold;
+  }
+  
+  /* allow scroll on the x-axis */
+  .scroll-wrapper {
+    overflow-x: auto;
+  }
+  
+  /* if a table is wrapped in a scroll wrapper,
+    the table cells shouldn't wrap */
+  .scroll-wrapper > table td {
+    white-space: nowrap;
+  }
+  
+  td {
+    text-align: center;
+  }
+  
+  .carousel-control-prev {
+    margin-left: -90px;
+    margin-top: -100px;
+  }
+  
+  .carousel-control-next {
+    margin-right: -80px;
+    margin-top: -100px;
+  }
+  
+  header {
+    border-radius: 25px;
+    background-color: #E0FFFF; 
+  }
+  
+  .grey {
+    background-color : #f2f2f2;
+  }
+  
+  text {
+    font-family: "Latin Modern", Georgia, Cambria, "Times New Roman", Times, serif;
+  }</style>`;
+
+  var svgBlob = new Blob([head, style, svgData, "</svg>"], {type:"image/svg+xml;charset=utf-8"});
+  return svgBlob;
+}
+
+export const get_tabs = (title, names, active) => {
+  let carousel = `<ul class="nav nav-tabs mb-3 nav-justified justify-content-center" id="${title}-pills-tab" role="tablist">`
+
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    name = name.replace(" ", "_");
+    carousel += `
+  <li class="nav-item" role="presentation">
+    <button class="nav-link ${i === active ? "active" : ""}" id="${title}-pills-${i}-tab" data-bs-toggle="pill" data-bs-target="#${title}-pills-${i}" type="button" role="tab" aria-controls="${title}-pills-${i}" aria-selected="true">${name.replace("_", " ")}</button>
+  </li>
+  `;
+  }
+  carousel += `</ul> <div class="tab-content" id="pills-tabContent">`;
+
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    name = name.replace(" ", "_");
+    carousel += `<div class="tab-pane fade show ${i === active ? "active" : ""}" id="${title}-pills-${i}" role="tabpanel" aria-labelledby="${title}-pills-${i}-tab">${name}</div>`
+  }
+  carousel += "</div>";
+  return carousel;
+}
+
+const next_rank = { k: "p", p: "c", c: "o", o: "f", f: "g", g: "s", s: "t" };
+const get_filter_function = (rank) => (line) =>
+  line.includes(rank.charAt(0) + "__") &&
+  !line.includes(next_rank[rank.charAt(0)] + "__") && 
+  true;
+
+const get_taxon_extractor = (rank) => (line) =>
+  new RegExp(String.raw`${rank.charAt(0)}__\w*`).exec(line)[0];
+
+// text is the metaphlan file. rank is the taxonomic rank to be extracted
+// returns an object where keys are taxons and values are abundances
+export const parse_file = (text, rank, idx) => {
+  const list = text.split("\n");
+
+  const filter_function = get_filter_function(rank);
+  const filtered = list.filter(filter_function);
+
+  const taxon_name = get_taxon_extractor(rank);
+  const abundance = (line) => parseFloat(line.match(/(\d)+\.(\d)+/g)[idx]) / 100;
+
+  const min_val = 0.00001;
+  const reducer = (prev, curr) => {
+    const name = taxon_name(curr);
+    const ab = abundance(curr);
+    if (ab < min_val) {
+      return { ...prev };
+    }
+    if (rank == "species" && !gmhi_model["features"].has(name)) {
+      return { ...prev };
+    }
+    return {
+      ...prev,
+      [name]: ab,
+    };
+  };
+  let obj = {};
+  try {
+    obj = filtered.reduce(reducer, {});
+  } catch (error) {
+    console.error(error);
+  }
+  return obj;
+};
+
+export const get_percentile = (values, score) =>
+  (
+    (100 * values.reduce((prev, curr) => prev + (score > curr ? 1 : 0), 0)) /
+    values.length
+  ).toFixed(2);
+
+  // for sample
+export const get_taxon_bar_list = (taxons) => {
+  return Object.keys(taxons).map(key => ({pop : "Sample", taxon : key, abundance : taxons[key]}));
+}
